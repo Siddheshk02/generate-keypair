1a46910a33153c9349cb701f666d55ade75fbb8b
Siddhesh Khandagale <khandagalesid02@gmail.com>
2024-08-24 20:56:16 +0530

    handle invalid address

--- check-balance.ts
+++ check-balance.ts
@@ -1,11 +1,15 @@
 import { Connection, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
 
-const publicKey = new PublicKey("the-public-key");
+const publicKey = new PublicKey("DaWV8GdRGvL4SFoRGmpbADoppvmsxPz8uEUr6xfmxdf4");
 
-const connection = new Connection("https://api.devnet.solana.com", "confirmed");
+if (!PublicKey.isOnCurve(publicKey.toBytes())) {
+    console.log("Invalid wallet address!!");
+} else {
+    const connection = new Connection("https://api.devnet.solana.com", "confirmed");
 
-const balanceInLamports = await connection.getBalance(publicKey);
+    const balanceInLamports = await connection.getBalance(publicKey);
 
-const balanceInSOL = balanceInLamports / LAMPORTS_PER_SOL;
+    const balanceInSOL = balanceInLamports / LAMPORTS_PER_SOL;
 
-console.log(`💰 Finished! The balance for the wallet at address ${publicKey} is ${balanceInSOL}!`)
+    console.log(`💰 Finished! The balance for the wallet at address ${publicKey} is ${balanceInSOL}!`)
+}
